55d35f138d07824e183efc6b8e5014487b16d619
Tak-Pesar <106800277+Tak-Pesar@users.noreply.github.com>
2023-09-30 21:24:48 +0330

    Create createTransaction.php

---
+++ example/createTransaction.php
@@ -0,0 +1,32 @@
+<?php
+
+use Tak\Tron\API;
+
+/*
+$tron = new API(privatekey : 'f13f3ef7734edd1355tf6cf1d90c78520d07388dad500c30322690d483f2efd1',wallet : 'TJZfm2PSQ38WNwzPqSBpTbVAynZpMEmfKR');
+or
+$tron = new API();
+$tron->setWallet('TJZfm2PSQ38WNwzPqSBpTbVAynZpMEmfKR');
+$tron->setPrivateKey('f13f3ef7734edd1355tf6cf1d90c78520d07388dad500c30322690d483f2efd1');
+ */
+
+$tron = new API(privatekey : 'f13f3ef7734edd1355tf6cf1d90c78520d07388dad500c30322690d483f2efd1',wallet : 'TJZfm2PSQ38WNwzPqSBpTbVAynZpMEmfKR');
+
+try {
+	$send = $tron->createTransaction(to : 'TJZfm2PSQ38WNwzPqSBpTbVAynZpMEmfKR',amount : 10.5,from : 'TJZfm2PSQ38WNwzPqSBpTbVAynZpMEmfKR');
+	if(isset($transaction->result) or $transaction->code === 'SUCCESS'):
+		print 'SUCCESS !';
+		print PHP_EOL;
+	endif;
+} catch(\Throwable $error){
+	exit($error->getMessage());
+}
+/*
+$send = $tron->createTransaction(to : 'TJZfm2PSQ38WNwzPqSBpTbVAynZpMEmfKR',amount : 10.5,from : 'TJZfm2PSQ38WNwzPqSBpTbVAynZpMEmfKR');
+or
+$send = $tron->sendTrx(to : 'TJZfm2PSQ38WNwzPqSBpTbVAynZpMEmfKR',amount : 10.5);
+or
+$send = $tron->sendTron(to : 'TJZfm2PSQ38WNwzPqSBpTbVAynZpMEmfKR',amount : 10500000,sun : true);
+ */
+
+print 'transaction : '.var_export($send,true);
