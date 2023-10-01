48d210c359fa0b37192802d1ca6dc8c052f65e34
Tak-Pesar <106800277+Tak-Pesar@users.noreply.github.com>
2023-10-01 16:12:12 +0330

    Update phrase.php

--- example/phrase.php
+++ example/phrase.php
@@ -4,12 +4,12 @@ use Tak\Tron\API;
 
 $tron = new API();
 
-$privatekey1 = 'f13f3ef7734edd1355tf6cf1d90c78520d07388dad500c30322690d483f2efd1';
+$privatekey1 = 'd2fd310eff9fc9ac6b4b65ad042bcc9a592847e7d9a21c66c4e8dd57d1e60f3d';
 
 $phrase = $tron->getPhraseFromPrivateKey($privatekey1);
-
-print $phrase;
 
 $privatekey2 = $tron->getPrivateKeyFromPhrase($phrase);
 
 var_dump($privatekey1 === $privatekey2);
+
+print $phrase;
