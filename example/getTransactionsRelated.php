0650825c3c1cddc7779f7dbf051dcede6901ba08
Tak-Pesar <106800277+Tak-Pesar@users.noreply.github.com>
2023-10-01 16:21:39 +0330

    Create getTransactions.php

---
+++ example/getTransactionsRelated.php
@@ -0,0 +1,20 @@
+<?php
+
+use Tak\Tron\API;
+
+$tron = new API();
+
+/*
+If the address parameter is not set, the address of the wallet added to the object will be used by default.
+$tron = new API(wallet : 'TJZfm2PSQ38WNwzPqSBpTbVAynZpMEmfKR');
+or
+$tron = new API();
+$tron->setWallet('TJZfm2PSQ38WNwzPqSBpTbVAynZpMEmfKR');
+
+@params string $address = null bool $confirmed = null bool $to = false bool $from = false bool $searchinternal = true int $limit = 20 string $order = 'desc' int $mintimestamp = null int $maxtimestamp = null
+@return mixed
+*/
+
+$transactions = $tron->getTransactionsRelated(address : 'TJZfm2PSQ38WNwzPqSBpTbVAynZpMEmfKR',confirmed : true,limit : 5);
+
+var_dump($transactions);
