fa56a33271054c960a40e975da283259f9f4b547
Tak-Pesar <106800277+Tak-Pesar@users.noreply.github.com>
2024-01-19 14:35:14 +0330

    Update getTransactions.php
    You can now take the list of transactions until you encounter the error of too many requests from the server :)
    To avoid this problem, you can set the limit to 200 and call sleep function in loop

--- example/getTransactions.php
+++ example/getTransactions.php
@@ -11,10 +11,20 @@ or
 $tron = new API();
 $tron->setWallet('TJZfm2PSQ38WNwzPqSBpTbVAynZpMEmfKR');
 
-@params string $address = null bool $confirmed = null bool $to = false bool $from = false bool $searchinternal = true int $limit = 20 string $order = 'desc' int $mintimestamp = null int $maxtimestamp = null
+@params string $address = null bool $confirmed = null bool $to = false bool $from = false bool $searchinternal = true int $limit = 20 string $order = 'block_timestamp,desc' int $mintimestamp = null int $maxtimestamp = null
 @return mixed
 */
 
 $transactions = $tron->getTransactionsRelated(address : 'TJZfm2PSQ38WNwzPqSBpTbVAynZpMEmfKR',confirmed : true,limit : 5);
 
 var_dump($transactions);
+
+/*
+You can use a loop to continue the list of transactions and get all of them
+*/
+
+foreach($transactions->iterator as $page => $transaction):
+	print 'Page '.strval($page + 1);
+	print PHP_EOL;
+	var_dump($transaction);
+endforeach;
